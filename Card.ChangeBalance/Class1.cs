c87de3fbfb7e7bc894ba9005368738e646a46394
Yana <Yana.ilina.1999@list.ru>
2021-11-30 19:19:40 +0300

    CardChangeBalance

---
+++ Card.ChangeBalance/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Card.ChangeBalance
+{
+    public class Class1
+    {
+        public double Balance { get; set; }
+        public string Name { get; set; }
+
+        public void ChangeBalance(double money)
+        {
+            Balance = Balance + money;
+        }
+    }
+}
