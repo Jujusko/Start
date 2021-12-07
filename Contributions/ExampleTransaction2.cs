60ae298518f4c1f6fdd09d884d97db0abbcf27a9
Yana <Yana.ilina.1999@list.ru>
2021-12-07 10:10:38 +0300

    Contributions

---
+++ Contributions/ExampleTransaction2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contributions
+{
+    class ExampleTransaction2 : AbstractTransaction
+    {
+        public override string Date { get; set; } // календарик для впф
+        public override string Name { get; set; }
+        public override double SummPlus { get; set; }
+        public override double SummMinus { get; set; }
+
+        public ExampleTransaction2(string date, string name, double summplus, double summminus)
+        {
+            Date = date;
+            Name = name;
+            SummPlus = summplus;
+        }
+    }
+}
